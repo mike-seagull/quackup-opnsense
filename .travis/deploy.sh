0f52c2a76818907db9913feaa981faa4a53d0de4
Mike Hollister <hollister.michael@gmail.com>
2019-02-19 11:22:00 -0600

    removed deployment files

--- .travis/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-scp -o LogLevel=quiet ./bin/quackup-opnsense ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/quackup-opnsense
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/quackup-opnsense"
